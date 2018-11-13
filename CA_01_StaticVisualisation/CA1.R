471d1c17212b1745338c1985c214bd7e7b103556
edelprior <edelprior@hotmail.com>
2018-11-13 20:53:25 +0000

    Question 4

--- CA_01_StaticVisualisation/CA1.R
+++ CA_01_StaticVisualisation/CA1.R
@@ -3,9 +3,36 @@ getwd();
  # facet_wrap(~Age +xlab("r") +
   #             ggtitle("Gender"))
 
+#library(ggplot2)
+# p1 <- qplot(gender, age, data = Influenza4D, color = cyl)
+# p2 <- qplot(gender, data = Influenza4D) + ggtitle("title")
+# p3 <- qplot(gender, data = Influenza4D, geom = "dotplot")
+# <-
+#  p1 + facet_wrap( ~ carb, nrow = 1) + theme(legend.position = "none") + ggtitle("facettedplot")
+
+# facet_grid(rows = NULL, cols = NULL, scales = fixed, space="fixed", shrink=TRUE, switch = NULL, 
+  #         drop= TRUE, margins = FALSE, facets = NULL)
+
+Influenza4D <= ggplot(data, aes(Patient,Type, Age, fill = Gender, colour))
+
+x + geom_boxplot() + facet_wrap(~Disease)
+
+xyz <= ggplot(data, aes(Patient.Type, Age, fill= Gender, color = Gender))
+xyz + geom_boxplot() + geom_violin() + geom_jitter()
+xyz + facet_wrap(~Disease)
+
+##__________________________________________________________________________________________________
+##__________________________________________________________________________________________________
+
+# Question Four
+cancer3D <- read.csv("~/Documents/GitHub/DataVisualisation/CA_01_StaticVisualisation/Data/cancer3D.csv", header=TRUE, sep=",")
+
 library(ggplot2)
-p1 <- qplot(gender, age, data = Influenza4D, color = cyl)
-p2 <- qplot(gender, data = Influenza4D) + ggtitle("title")
-p3 <- qplot(gender, data = Influenza4D, geom = "dotplot")
-<-
-  p1 + facet_wrap( ~ carb, nrow = 1) + theme(legend.position = "none") + ggtitle("facettedplot")
+ggplot(canctype,aes("",sex))+ geom_boxplot() +
+  facet_wrap(~age_group)+xlab("Label") +
+  ggtitle("Title")
+
+
+ggplot(canctype, aes("",Number))+geom_boxplot(fill = "red")+ facet_wrap(~Apprentice,ncol = 6,scales = "free") + theme(axis.text.x=element_text(angle = -90,size =5))+theme(strip.background = element_rect(fill = "grey20",colour = "grey80",size =1),strip.text = element_text(colour = "white"))
+t.test(cancer3D$ canctype, cancer3D$age_group,paired = TRUE)
+
